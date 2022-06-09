bc98260b65db5e1e021a7a626a709e42a7544005
caleb-freitas <calebfreitas@tutanota.com>
2022-06-09 20:04:53 -0300

    refactor: added Avatar component and use it on Comment and Post components

--- ignite-feed/src/components/Sidebar.jsx
+++ ignite-feed/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css'
 import { PencilLine } from "phosphor-react"
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
   return (
@@ -7,7 +8,7 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1518640467707-6811f4a6ab73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWluaW1hbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/caleb-freitas.png" />
+        <Avatar src="https://github.com/caleb-freitas.png" />
         <strong>Caleb Freitas</strong>
         <span>Software Developer</span>
       </div>
