fd4986f22a2e6b43c543d5d60926d02376df923e
caleb-freitas <calebfreitas@tutanota.com>
2022-06-10 15:32:51 -0300

    feat: added comment feature

--- ignite-feed/src/components/Comment.jsx
+++ ignite-feed/src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from "phosphor-react"
 import { Avatar } from "./Avatar"
 import styles from "./Comment.module.css"
 
-export function Comment() {
+export function Comment(props) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/caleb-freitas.png" />
@@ -19,7 +19,7 @@ export function Comment() {
               <Trash size={24} />
             </button>
           </header>
-          <p>Comment content</p>
+          <p>{props.content}</p>
         </div>
         <footer>
           <button>
--- ignite-feed/src/components/Post.jsx
+++ ignite-feed/src/components/Post.jsx
@@ -1,14 +1,33 @@
-import { Comment } from './Comment'
-import { Avatar } from './Avatar'
-import styles from './Post.module.css'
+import { useState } from 'react'
 import { format, formatDistanceToNow } from "date-fns"
 
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
+import styles from './Post.module.css'
+
 export function Post(props) {
+  const [comments, setComments] = useState(["Post muito massa"])
+
+  const [newCommentText, setNewCommentText] = useState('')
+
   const { author, content, publishedAt } = props
+
   const publishedDateFormatted = format(publishedAt, "LLL d 'at' h:mm aaa")
+
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     addSuffix: true
   })
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+    setComments([...comments, newCommentText])
+    setNewCommentText('')
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -42,17 +61,17 @@ export function Post(props) {
           </p>
         </p>
       </div>
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Leave your feedback!</strong>
-        <textarea placeholder='Leave your comment'></textarea>
+        <textarea name='comment' placeholder='Leave your comment' onChange={handleNewCommentChange} value={newCommentText}></textarea>
         <footer>
           <button type='submit'>Publish</button>
         </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment content={comment} />
+        })}
       </div>
     </article>
   )
